dbf8d946b314ca92a8ffe3d5c47621f22ab3d9fa
herouchsaad <herouchsaad@gmail.com>
2026-08-09 14:32:25 +0100

    test: trigger CI/CD after CD workflow fix

--- intern-management-system-frontend/src/components/LayoutComponent.tsx
+++ intern-management-system-frontend/src/components/LayoutComponent.tsx
@@ -63,7 +63,7 @@ const [notifications, setNotifications] = useState<Notification []>();
 const axiosPrivate = useAxiosPrivate();
 const navigate = useNavigate();
 
-    const title = "Intern Management System - CI/CD Test";
+    const title = "Intern Management System - CD Retest";
     const footer = "IMS ©2023"
 
 
